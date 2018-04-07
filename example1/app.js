4990ea1bdcba0c0c93a169d79683b3aca7663977
Nathan Stanford Sr <Nathan.Stanford@gmail.com>
2018-04-07 12:20:01 -0400

    Added a parameter to the function greet

--- example1/app.js
+++ example1/app.js
@@ -5,11 +5,10 @@ data: {
   firstName: 'Nathan',
   lastName: 'Stanford',
   job: 'Programmer',
-  message: 'World!'
 },
 methods: {
-  greet: function() {
-    return 'Hello ' + this.message;
+  greet: function(message) {
+    return 'Hello ' + message;
   }
 }
 

3cff0b30eb60a4cb817846287e8bd754beeaedd3
Nathan Stanford Sr <Nathan.Stanford@gmail.com>
2018-04-07 15:08:01 -0400

    Tracking the mousemove event.

--- example2/app.js
+++ example2/app.js
@@ -2,9 +2,21 @@
 new Vue({
   el: '#app',
   data: {
-    age:25
+    age: 25,
+    x: 0,
+    y: 0
   },
   methods: {
+    add: function(inc) {
+      this.age+=inc;
+    },
+    subtract: function(dec) {
+      this.age-=dec;
+    },
+    updateXY: function(event) {
+      this.x = event.offsetX,
+      this.y = event.offsetY
+    }
   
     }
   });

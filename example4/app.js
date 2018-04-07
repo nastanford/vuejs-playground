c266fde9ac85ddf4f62bc06d3c143e22e90bc4ad
Nathan Stanford Sr <Nathan.Stanford@gmail.com>
2018-04-07 15:39:55 -0400

    Added Starter Code and Two way binding

---
+++ example4/app.js
@@ -0,0 +1,17 @@
+
+new Vue({
+  el: '#app',
+  data: {
+    name: '',
+    age: ''
+  },
+  methods: {
+    logName: function() {
+      console.log('you entereed your name.')
+    },
+    logAge: function() {
+      console.log('you entereed your age.')
+    },
+  }
+});
+  

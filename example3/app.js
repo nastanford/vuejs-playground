13c6d8094003a2096e0751da30cdeea0ddca5cdf
Nathan Stanford Sr <Nathan.Stanford@gmail.com>
2018-04-07 15:22:58 -0400

    Keyboard Events

---
+++ example3/app.js
@@ -0,0 +1,15 @@
+
+new Vue({
+  el: '#app',
+  data: {
+  },
+  methods: {
+    logName: function() {
+      console.log('you entereed your name.')
+    },
+    logAge: function() {
+      console.log('you entereed your age.')
+    },
+  }
+});
+  

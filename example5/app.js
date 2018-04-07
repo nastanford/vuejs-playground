50438ee30d0a188e6964a572746d1796a23dc45f
Nathan Stanford Sr <Nathan.Stanford@gmail.com>
2018-04-07 15:57:34 -0400

    console.log for each item.

--- example5/app.js
+++ example5/app.js
@@ -8,9 +8,11 @@ new Vue({
   },
   methods: {
     // addToA: function() {
+    // console.log("Add to A");
     //   return this.a + this.age;
     // },
     // addToB: function() {
+    // console.log("Add to B");
     //   return this.b + this.age;
     // }
   },
